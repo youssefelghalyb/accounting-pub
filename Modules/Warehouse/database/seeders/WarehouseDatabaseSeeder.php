7d7de9acc54afe9753bfc39a52e7d3c3af298920
youssefelghalyb <yousef.elghalyb@gmail.com>
2025-11-18 06:39:24 +0200

    Warehouse Module

---
+++ Modules/Warehouse/database/seeders/WarehouseDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Warehouse\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class WarehouseDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
